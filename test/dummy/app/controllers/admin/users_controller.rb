f05c6f63386f58e5a5b3c7c601c9f45adcffae0c
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-12 15:33:43 +0200

    doing rails ajax way

--- test/dummy/app/controllers/admin/users_controller.rb
+++ test/dummy/app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < AdminController
+  before_action :all_user, only: [:index, :create, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   cruds_express_model User, cruds: [:create, :update, :destroy, :show], controller: 'admin/users',
     permit: [:first_name, :last_name, :email, :birthday, :gender], hide: []
   cruds_express_model User, cruds: :read, source: :user_list
@@ -13,13 +16,21 @@ class Admin::UsersController < AdminController
   #   # @cruds_helper = self.class
   # end
 
-  def user_list
-    User.all
+  def all_user
+    @entries = User.all
   end
 
-  def article_list
-    Article.all
+  def set_user
+
   end
+
+  # def article_list
+  #   Article.all
+  # end
+  #
+  # def user_list
+  #   @entries = User.all
+  # end
 
 
 end
--- test/dummy/app/controllers/admin_controller.rb
+++ test/dummy/app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  respond_to :html, :js
   cruds_expresslize
   before_action :cruds_express_prepare
 
