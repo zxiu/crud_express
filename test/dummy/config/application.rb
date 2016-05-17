d24256070aeced30be0301e78f8c980d68c7dcc2
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-17 09:42:21 +0200

    add font of bootstrap

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -21,6 +21,6 @@ module Dummy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
-
