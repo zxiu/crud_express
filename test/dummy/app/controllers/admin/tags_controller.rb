68948100497c8b7232d87b129449a12adb785087
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-17 16:34:34 +0200

    add tags controller

---
+++ test/dummy/app/controllers/admin/tags_controller.rb
@@ -0,0 +1,10 @@
+class Admin::TagsController < ModelController
+  cruds_express_roller model: Tag
+  cruds_express_entries :list
+  before_action :prepare_cruds_express
+  around_action :respond_curds_express
+  def list
+    Tag.all
+  end
+
+end
