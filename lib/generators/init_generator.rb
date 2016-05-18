f40ad6f996a63b93a14ca89cd10261843b448ce1
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-18 16:06:32 +0200

    add first generator

--- lib/generators/init_generator.rb
+++
@@ -1,9 +0,0 @@
-module Cruds_express
-  module Generators
-    class InitGenerator < Rails::Generators::NamedBase
-      def copy_view_templates
-        create_file "app/views/shared/test.html", "<html></html>"
-      end
-    end
-  end
-end
