edc71551b38d2652d4320cab6852a6ab7c709481
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-18 16:25:44 +0200

    commit before test copy directory

--- lib/generators/cruds_express/init_generator.rb
+++ lib/generators/cruds_express/init_generator.rb
@@ -1,11 +1,12 @@
 module Cruds_express
   module Generators
     class InitGenerator < Rails::Generators::Base
-      desc "Copy SimpleForm default files"
+      desc "Copy templates view file"
       source_root File.expand_path('../templates', __FILE__)
       def copy_view_templates
-        puts("copy_view_templates")
+        puts("source_root=#{self.class.source_root}")
         create_file "app/views/shared/test.html", "<html></html>"
+        directory "views"
       end
     end
   end
