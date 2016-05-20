29e4f8700b0cc3b2ea0791edd70fcf16d2b238ab
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-20 15:29:54 +0200

    small fix

--- test/dummy/app/controllers/admin/users_controller.rb
+++ test/dummy/app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ModelController
   crud_express role: :model, model: User, collection: :list, includes: {:articles => {:label => :title}}, hide: [:created_at]
 
   def list
-    User.includes(:articles).all
+    User.all
   end
 
 end
